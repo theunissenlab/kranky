80f64aa92249c7a0106ae53f9d85768affe102f4
Jeff Knowles <jknowles@Jeffs-MacBook-Pro.local>
2015-06-22 11:35:42 -0700

    functioning loop. eta 1 day

--- kranky.py
+++ kranky.py
@@ -1,17 +1,23 @@
-# load rc file
-# load and verify stimuli
-# generate stimuli, triggers, and write rec file
-# que trigger (and recording in future)
+import numpy as np
+import scipy as sp
 
 
 
-def load_rc file(fname):
+def load_rc_file(fname):
     params = {}
     stimset = {}
+    with open(fname) as rcfid:
+        for line in rcfid:
+            print line
+    params['ntrials']= 10
     return params, stimset
 
 def generate_playback_plan(params, stimset):
     playback_plan = {}
+    playback_plan['trials']=[]
+    for ktrial in range(0,params['ntrials']):
+        trial = {}
+        playback_plan['trials'].append(trial)
     return playback_plan
 
 def write_playback_audio_file(params, stimset, playback_plan):
@@ -19,6 +25,7 @@ def write_playback_audio_file(params, stimset, playback_plan):
     # open output wav file
     # begin iterating thru trials of playback plan.
     for trial in playback_plan['trials']:
+        print trial
         # generate trigger and load stimulus
         # write data to output file
         # write to .rec.paf
@@ -44,15 +51,20 @@ def run_playback(params, stimset, playback_plan):
                 # write messages to .rec file
                 pass
             pass
-        # write exit note to .rec.pb file
+        # write exit note to .rec file
 
             
     except Exception as e:
-        # empty .rec buffer then close rec file with error note
+        # empty .rec buffer 
+        # close rec file with error note
         raise(e) 
-
     pass
 
 
 if __name__=="__main__":
     rc_fname = "./test.rc"
+    params, stimset = load_rc_file(rc_fname)
+    playback_plan = generate_playback_plan(params,stimset)
+    write_playback_audio_file(params, stimset, playback_plan)
+    # run_playback(params, stimset, playback_plan)
+
