dacb049ac0c0b7b70379cf9275d4c9a40621c187
Jeff Knowles <jknowles@Jeffs-MacBook-Pro.local>
2015-06-22 13:26:24 -0700

    latest

--- kranky.py
+++ kranky.py
@@ -2,13 +2,20 @@ import numpy as np
 import scipy as sp
 
 
-class TrialBuffer(object):
+class PlaybackBuffer(object):
     pass
+
+
+default_params = {}
+default_params['ao_freq'] = 44100
+default_params['ai_freq'] = 32000
+default_params['n_trials'] = 1000
+default_params['stim_order'] = 2
 
 
 
 def load_rc_file(fname):
-    params = {}
+    params = dict(default_params)
     stimset = {}
     stimset['stims'] = []
     with open(fname) as rcfid:
@@ -24,6 +31,7 @@ def parse_rc_line(line, params, stimset):
                 stim = {}
                 stim['fname'] = parts[2]
                 stim['name'] = stim['fname']
+                verify_stim(stim)
                 stimset['stims'].append(stim)
                 stimset['stims'][len(stimset['stims'])-1]['stim_idx'] = len(stimset['stims'])-1
         elif parts[0].lower() == "set":
@@ -31,28 +39,32 @@ def parse_rc_line(line, params, stimset):
                 params[parts[1]] = int(parts[2]) ###### make this parse different formats 
     return params, stimset # for transparency
 
+def verify_stim(stim):
+    pass
+
 def generate_playback_plan(params, stimset):
     playback_plan = {}
     playback_plan['trials']=[]
 
     trial_list = np.array([])
-    if params['stim_order'] == 0:
+    if params['stim_order'] == 0: # generate trials in given order
         while len(trial_list) < params['n_trials']:
             trial_list = np.append(trial_list, np.arange(0,len(stimset['stims'])))
-    elif params['stim_order'] == 2:
+    elif params['stim_order'] == 1:
+        raise(Excpetion('not supported yet'))
+    elif params['stim_order'] == 2: # generate randomly shuffeled order 
         while len(trial_list) < params['n_trials']:
             trial_list = np.append(trial_list, np.random.shuffle(np.arange(0,len(stimset['stims']))))
     trial_list = trial_list[0:params['n_trials']]
 
-    
-
+    # generate trials according to plan
     for ktrial in range(0,params['n_trials']):
         trial = {}
         trial['trial_idx'] = ktrial
         trial['stim_idx'] = int(trial_list[ktrial])
         trial['stim'] = stimset['stims'][trial['stim_idx']]
         playback_plan['trials'].append(trial)
- 
+
     return playback_plan
 
 def write_playback_audio_file(params, stimset, playback_plan):
@@ -60,9 +72,7 @@ def write_playback_audio_file(params, stimset, playback_plan):
     # open output wav file
     # begin iterating thru trials of playback plan.
     for ktrial, trial in enumerate(playback_plan['trials']):
-        print ktrial, trial
-
-
+        # print ktrial, trial['stim']['name']
         # generate trigger and load stimulus
         # write data to output file
         # write to .rec.paf
@@ -77,13 +87,14 @@ def run_playback(params, stimset, playback_plan):
     # begin iterating thru trials of playback plan.
     ktrial = 0
     Playing = True
+    pb = PlaybackBuffer()
     try: 
         while Playing:
-            if tb.ready:
+            if pb.tb_ready:
                 # generate trigger and load stimulus
                 # add trial to tb
                 pass
-            if rb.buffer():
+            if pb.rbuffer():
                 # read messages from buffer
                 # write messages to .rec file
                 pass
@@ -102,7 +113,6 @@ if __name__=="__main__":
     rc_fname = "./test.rc"
     params, stimset = load_rc_file(rc_fname)
     print params
-    print stimset
     playback_plan = generate_playback_plan(params,stimset)
     write_playback_audio_file(params, stimset, playback_plan)
     # run_playback(params, stimset, playback_plan)
