76d9467e9f8bd4a601260762320e5afa84c327c7
Jeffrey Knowles <jknowles@albatross.cin.ucsf.edu>
2015-10-23 14:41:38 -0700

    stuff

--- comedi_ao/ao_slave.c
+++ comedi_ao/ao_slave.c
@@ -16,3 +16,73 @@
 #include <readline/history.h>
 #include <signal.h>
 #include "ao_slave.h"
+
+
+/* stimulus */
+
+#define MAX_FILENAME_LENGTH 200
+int stim_init(void);
+void stim_output(char *buf, int len);
+void stim_output2(char *buf, int len);
+
+
+
+void ao_init(void)
+{
+	int i;
+	int err;
+
+	ao_buf_len = AO_FIFO_LEN;
+	ao_buf = realloc(ao_buf,ao_buf_len);
+
+	ao_subd = 1;
+	ao_maxdata = comedi_get_maxdata(dev,ao_subd,0);
+	ao_range_index = AO_RANGE_INDEX;
+	ao_range = comedi_get_range(dev,ao_subd,0,ao_range_index);
+	if (debug > 0) printf("ao_range (volts): %g : %g.\n", ao_range->min, ao_range->max);
+
+	memset(&ao_cmd, 0, sizeof(ao_cmd));
+
+	ao_cmd.subdev = ao_subd;
+	ao_cmd.flags = trig_rt;
+	ao_cmd.start_src = TRIG_INT;
+	ao_cmd.start_arg = 0;
+	ao_cmd.scan_begin_src = TRIG_TIMER;
+	ao_cmd.scan_begin_arg = 1e9/ao_freq;
+	ao_cmd.convert_src = TRIG_NOW;
+	ao_cmd.convert_arg = 0;
+	ao_cmd.scan_end_src = TRIG_COUNT;
+	ao_cmd.scan_end_arg = 1;
+	ao_cmd.stop_src = TRIG_NONE;
+	ao_cmd.stop_arg = 0;
+
+	ao_cmd.chanlist = ao_chanlist;
+	ao_cmd.chanlist_len = 1;
+
+	ao_chanlist[0] = CR_PACK(0, ao_range_index, AREF_GROUND);
+
+	err = comedi_command_test(dev, &ao_cmd);
+	if(err != 0){
+		printf("bad ao cmd\n");
+	}
+
+	if(debug>=1){
+		fprintf(stderr,"ao: test returned %d (%s)\n",err,
+			cmdtest_messages[err]);
+		dump_cmd(stderr,&ao_cmd);
+	}
+
+	ao_freq = 1e9/ao_cmd.scan_begin_arg;
+	printf("actual AO rate = %g\n",ao_freq);
+
+	err = comedi_command(dev, &ao_cmd);
+	if(err != 0){
+		comedi_perror("ao comedi_command");
+	}
+
+	stim_output2(ao_buf, ao_buf_len/2);
+
+	for(i=0;i<5;i++){
+		output_some();
+	}
+}
--- comedi_ao/ao_slave.h
+++ comedi_ao/ao_slave.h
@@ -1,66 +1,14 @@
 
-#ifndef _SCOPE_H_
-#define _SCOPE_H_
-
 #include <stdio.h>
 #include <glib.h>
 
-#ifdef NI625x
-  /* For the NI625x M-series board (length in bytes) */
-  #define AO_SUBD 1
-  #define AO_FIFO_LEN 16382
-  #define DIO_SUBD 7
-  #define MAX_CHANS 32
-  /* Use +- 5V range on M-series board because of impedance 
-     load clipping issues. */
-  #define AO_RANGE_INDEX 1
-#else
-  /* For the NI6052E board */
-  #define AO_SUBD 1
-  #define AO_FIFO_LEN 2048
-  #define DIO_SUBD 7
-  #define MAX_CHANS 16
-  #define AO_RANGE_INDEX 0
-#endif
-
-struct _scope_chan {
-	int show;
-	double offset;
-	char name[100];
-        int index;
-        int triggered;
-        double trig_level;
-        double trig_level2;
-        int trig_type;
-};
-typedef struct _scope_chan scope_chan;
-
-struct _stim_struct {
-	char *name;
-	char *basename;
-	char *data;
-	int n_samples;
-	int index;
-	int flags;
-};
-typedef struct _stim_struct stim_struct;
-
-struct _display_struct {
-        stim_struct *stim;
-        scope_chan *chan;
-        int index;
-        int is_displayed;
-};
-
-typedef struct _display_struct display_struct;
 
 /* global variables */
 
 extern int krank_selected_channel;
 extern int krank_selected_display;
 extern int krank_selected_stimulus;
-/* Global hash of raster display configurations. */
-extern GHashTable *krank_displays;
+
 
 extern double ai_freq;
 extern double ao_freq;
@@ -82,7 +30,6 @@ extern double attenuation;
 extern double attenuation2;
 
 /* scope.c */
-
 extern int streaming_on;
 extern int ai_n_chan;
 extern comedi_range *ai_range;
@@ -90,8 +37,8 @@ extern lsampl_t ai_maxdata;
 extern scope_chan scope_chans[];
 
 void stop(char *why);
-void plotupdate(void);
-void dump_comments(FILE *out);
+// void plotupdate(void);
+// void dump_comments(FILE *out);
 int check_file_exists(char *path);
 extern int n_comments;
 void scope_log(char *str);
@@ -106,31 +53,31 @@ void vars_dump(FILE *out);
 extern int n_stims;
 extern stim_struct *stims[];
 
-void stim_plotdata_all(void);
+// void stim_plotdata_all(void);
 void stim_stop(char *why);
 void stimdata_reset(void);
-void print_date_rfc822(char *s, struct timeval *tv);
+// void print_date_rfc822(char *s, struct timeval *tv);
 
 /* display.c */
-display_struct* display_new(void);
-void display_free(display_struct *disp);
-void display_free_adaptor(gpointer value);
-void display_set_stim(display_struct *disp, stim_struct *stim);
-void display_set_chan(display_struct *disp, scope_chan *chan);
-void display_reset_stim(display_struct *disp);
-void display_reset_stim_adaptor(gpointer key, gpointer value, gpointer user_data);
-display_struct *display_lookup(int idx);
-display_struct *display_select(int idx);
-int display_init(display_struct *disp);
-int display_show(display_struct *disp);
-display_struct *display_create(scope_chan *chan, stim_struct *stim);
-void display_destroy(display_struct *disp);
-display_struct *display_clone(display_struct *disp_ref);
-int display_clone_init(display_struct *disp, display_struct *disp_ref);
-int display_clone_show(display_struct *disp, display_struct *disp_ref);
-int display_is_valid(display_struct *disp);
-int displays_get_last_index(void);
+// display_struct* display_new(void);
+// void display_free(display_struct *disp);
+// void display_free_adaptor(gpointer value);
+// void display_set_stim(display_struct *disp, stim_struct *stim);
+// void display_set_chan(display_struct *disp, scope_chan *chan);
+// void display_reset_stim(display_struct *disp);
+// void display_reset_stim_adaptor(gpointer key, gpointer value, gpointer user_data);
+// display_struct *display_lookup(int idx);
+// display_struct *display_select(int idx);
+// int display_init(display_struct *disp);
+// int display_show(display_struct *disp);
+// display_struct *display_create(scope_chan *chan, stim_struct *stim);
+// void display_destroy(display_struct *disp);
+// display_struct *display_clone(display_struct *disp_ref);
+// int display_clone_init(display_struct *disp, display_struct *disp_ref);
+// int display_clone_show(display_struct *disp, display_struct *disp_ref);
+// int display_is_valid(display_struct *disp);
+// int displays_get_last_index(void);
 
 
-#endif
+// #endif
 
