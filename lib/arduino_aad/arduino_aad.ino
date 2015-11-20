478a08bc4f12e343dd04e28d9babbd18555b827b
Jeffrey Knowles <jknowles@albatross.cin.ucsf.edu>
2015-11-20 12:08:34 -0800

    new stuff

---
+++ lib/arduino_aad/arduino_aad.ino
@@ -0,0 +1,68 @@
+
+#define FASTADC 1
+#define DOTIMER 1
+// defines for setting and clearing register bits
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+
+
+byte output_pins[8] = {7,8,9,10,11,12,13};
+byte analog_pin = 0;
+byte analogval = 0;
+#if DOTIMER
+long count = 0;
+long timercount = 100000;
+long lasttime=0; 
+#endif
+
+void setup()
+{
+#if FASTADC
+  // set prescale to 16
+  cbi(ADCSRA,ADPS2) ;
+  sbi(ADCSRA,ADPS1) ;
+  sbi(ADCSRA,ADPS0) ;
+#endif
+#if DOTIMER
+  Serial.begin(19200);
+#endif
+  // set portB (output pins) to output and low
+  for (int i = 0; i < sizeof(output_pins); i++) {
+    pinMode(output_pins[i], OUTPUT);
+    digitalWrite(output_pins[i], 0);
+  }
+}
+
+
+void loop(){
+#if DOTIMER
+  count = count + 1;
+  if (count >= timercount)
+  {
+    Serial.println((millis()-lasttime)*1000/timercount);
+    Serial.println(analogval);
+    for (int i = 0; i < 8; i++){
+          Serial.print(bitRead(analogval,i));
+        }
+    Serial.println();
+    lasttime = millis();
+    count = 0; 
+  }
+#endif
+
+//  analogval = analogRead(analog_pin)>>6;
+  analogval = B00100001;
+
+  PORTB = analogval<<2;
+  
+
+
+
+
+}
+
---
+++ lib/pyjack_test.py
@@ -0,0 +1,6 @@
+import jack as jack
+
+import ipdb; ipdb.set_trace()
+test_client=jack.Client("Test")
+test_client.actovate()
+import ipdb; ipdb.set_trace()
